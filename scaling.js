11326117eadf960165b97d3c92912c7ba398bf19
matt <mattyysekk@gmail.com>
2020-12-20 21:35:00 +0100

    Create scaling.js

---
+++ scaling.js
@@ -0,0 +1,9 @@
+if (screen.width >= 1920) {
+	
+	document.write("<style>.registerHeader{font-size: 48px;}</style>")
+}
+
+if (screen.width >= 1680) {
+	
+document.write("<style>.registerHeader{margin-left: 230px; font-size: 48px;}.loginHeader{margin-left: 552px; font-size: 48px;}.loginParagraph{margin-left: 539px;}.loginButton{margin-left: 563px;}body{max-height: 798px; max-width: 1080px; margin-top: 130px;}.loginHeader:hover{font-size: 52px;}.registerHeader:hover{font-size: 52px;}.registerButton{margin-left: 281px;}</style>")
+}
